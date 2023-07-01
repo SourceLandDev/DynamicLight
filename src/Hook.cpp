eb1787a4b80652f5cbe52466723e943fcbff43ab
星梦 <starsdream00@icloud.com>
2023-07-01 13:12:05 +0800

    Update Hook.cpp

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -36,8 +36,8 @@ TInstanceHook(void, "?explode@Level@@UEAAXAEAVBlockSource@@PEAVActor@@AEBVVec3@@
 
 // Tick
 
-TInstanceHook(void, "?normalTick@Mob@@UEAAXXZ",
-              Mob) {
+TInstanceHook(void, "?normalTick@Player@@UEAAXXZ",
+              Player) {
     original(this);
     if (!config.isEnabled() || !hasDimension())
         return;
